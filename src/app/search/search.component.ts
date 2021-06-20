6df700709aeae540dd6436b10558b981ced1b31f
Martino Mensio <martino.mensio@open.ac.uk>
2021-06-20 10:09:52 +0200

    most popular entries stub

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-search',
@@ -8,10 +11,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SearchComponent implements OnInit {
 
   @Input() expanded: boolean = false;
+  most_popular_entries = [];
+  counter = 0;
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    // Animate Search Suggestions
+    // var terms = $("ul.animate-text li");
+    this.apiService.getMostPopularEntries().subscribe((result: any) => {
+      this.most_popular_entries = result;
+      console.log(this.most_popular_entries);
+    })
+
+    // const rotateTerm = () => {
+    //     // var ct = $("#rotate").data("term") || 0;
+    //     // console.log(terms.eq([ct]).text());
+    //     $("#rotate").data("term", ct == this.most_popular_entries.length - 1 ? 0 : ct + 1).text(terms.eq([ct]).text()).fadeIn(800).delay(5000).fadeOut(800, rotateTerm);
+    // }
+    // $(rotateTerm);
   }
 
 }
