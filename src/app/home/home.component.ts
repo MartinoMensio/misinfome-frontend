03f4b116662602b9b7c6e157c820cb2f7f15d3a2
Martino Mensio <martino.mensio@open.ac.uk>
2021-09-28 10:47:47 +0100

    updated frontend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { ApiService, LoadStates } from '../api.service';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +8,22 @@ import { ApiService } from '../api.service';
 })
 export class HomeComponent implements OnInit {
   
-  loading = true;
+  loadStates = LoadStates;
+  load_state = LoadStates.None;
   data: any;
-  countUpOptions={duration:5};
+  error_detail = '';
+  countUpOptions={duration:5, decimalPlaces:2};
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.load_state = LoadStates.Loading;
     this.apiService.getHome().subscribe(homeData => {
       this.data = homeData;
-      this.loading = false;
+      this.load_state = LoadStates.Loaded;
+    }, error => {
+      this.load_state = LoadStates.Error;
+      this.error_detail = error.error.detail;
     })
   }
 
